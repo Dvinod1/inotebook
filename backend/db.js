57128f2a8aff75f234407a422b6476385729bc8f
Dvinod1 <107790735+Dvinod1@users.noreply.github.com>
2023-02-21 12:23:27 +0530

    bcrypt_Jwt_token

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose');
 
 
-const mongoURI="mongodb://127.0.0.1:27017/"
-
+const mongoURI="mongodb://127.0.0.1:27017/INotebook1"
+ 
 const connecToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
         console.log("connected to mongo successfully")
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,15 +2,59 @@
 const express =require('express');
 const User=require('../models/User')
 const router=express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
-//create a user using : POST  "/api/auth".  Doesnt requires auth
-router.post('/',(req, res)=>{
- console.log(req.body);
- const user=User(req.body)
- user.save()
- res.send(req.body)
- 
-})
+const JWT_SECRET='hellohowareyou123'
+
+//create a user using : POST  "/api/auth/createuser".  No login required  
+router.post('/createuser',[
+    body('email','enter a valid name').isEmail(),
+    body('name','enter a valid email').isLength({min:4}),
+    body('password','password must be 4 charactor').isLength({min:4}),
+],
+//if there are errors , return Bad request and the errors
+ async(req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+ }
+
+//cheak wheather the user with this email is already exists.
+try {
+let user=await User.findOne({email:req.body.email});
+if (user){
+  return res.status(400).json({error:"Sorry a user with this email already exists"})
+}
+
+const salt =await bcrypt.genSalt(10);
+ secpass= await bcrypt.hash(req.body.password, salt);
+
+//creating a new user
+user =await User.create({
+    name: req.body.name,
+    password: secpass,
+    email: req.body.email,
+  })
+
+const data={
+  user:{
+    id:user.id
+  }
+}
+const authToken=jwt.sign(data,JWT_SECRET);
+console.log(authToken);
+  // res.json(user)
+  res.json({authToken})
+
+} 
+catch (error) {
+  console.error(error.message);
+  res.status(500).json("Some error occoured")
+}
+
+},)
 
 module.exports=router
